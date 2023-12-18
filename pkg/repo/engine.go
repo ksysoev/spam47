29e10343e678982c3f133823fae6bde674503f34
Кирилл Сысоев <k.sysoev@me.com>
2023-12-18 21:02:16 +0800

    Implements methods for saving and loading state from redis

--- pkg/repo/engine.go
+++ pkg/repo/engine.go
@@ -1,11 +1,16 @@
 package repo
 
-import "github.com/redis/rueidis"
+import (
+	"context"
+	"time"
+
+	"github.com/redis/rueidis"
+)
 
 type EngineRepo interface{}
 
 type EngineRedisRepo struct {
-	redis any
+	redis rueidis.Client
 }
 
 func NewEngineRedisRepo() (*EngineRedisRepo, error) {
@@ -18,4 +23,23 @@ func NewEngineRedisRepo() (*EngineRedisRepo, error) {
 	return &EngineRedisRepo{
 		redis: redis,
 	}, nil
+}
+
+func (e *EngineRedisRepo) Save(class string, data []string) error {
+	cmd := e.redis.B().Sadd().Key(class).Member(data...).Build()
+	e.redis.Do(context.Background(), cmd)
+	return nil
+}
+
+func (e *EngineRedisRepo) Load(class string) (data []string, err error) {
+	// This provides Redis client-side caching support.
+	// If data is updated in redis, cache will be invalidated.
+	cmd := e.redis.B().Smembers().Key(class).Cache()
+	resp := e.redis.DoCache(context.Background(), cmd, time.Hour)
+
+	if err := resp.Error(); err != nil {
+		return nil, err
+	}
+
+	return resp.AsStrSlice()
 }
